08523cc4705b6e439bf0d2b3be2ba05c0eb90214
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-02-27 21:12:03 +0100

    printing table on homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,34 @@
     @vite('resources/js/app.js')
 </head>
 <body>
-    <h1>TRENI</h1>
+    <h1>TRENI IN PARTENZA OGGI</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>Compagnia</th>
+                <th>Stazione di partenza</th>
+                <th>Stazione di arrivo</th>
+                <th>Orario di partenza</th>
+                <th>Orario di arrivo</th>
+                <th>Codice treno</th>
+                <th>Numero vagoni</th>
+                <th>Cancellato</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($trains as $train)
+            <tr>
+                <td>{{ $train->company_name }}</td>
+                <td>{{ $train->departure_station }}</td>
+                <td>{{ $train->arrival_station }}</td>
+                <td>{{ $train->departure_time }}</td>
+                <td>{{ $train->arrival_time }}</td>
+                <td>{{ $train->train_code }}</td>
+                <td>{{ $train->numbers_of_wagons }}</td>
+                <td>{{ $train->is_deleted ? 'Sì' : 'No' }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 </body>
 </html>
